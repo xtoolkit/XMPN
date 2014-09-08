c277e7f39ae667f46ca297b76e64c79347109fd1
Arshen7 <delta.dorna@gmail.com>
2014-09-08 14:33:57 +0430

    flat theme
    add flat theme for Xaparat

---
+++ XMoreOption/modules/XMoreOption/theme/Xaparat/Flat/xatheme.php
@@ -0,0 +1,22 @@
+﻿<?php
+function xa_theme($id,$mod){
+global $prefix, $db, $dbname;
+?><link rel="stylesheet" href="modules/XMoreOption/theme/Xaparat/Flat/css/style.css" type="text/css" /><?php
+$result = $db->sql_query("SELECT *
+FROM `" . $prefix . "_xaparat`
+WHERE `xamid` =$id
+AND `xamod` LIKE '$mod'
+LIMIT 0 , 30");
+	while ($row = $db->sql_fetchrow($result)) {
+	mb_internal_encoding('UTF-8');
+	$xaid = intval($row['xaid']);
+	$xatag = $row['xatag'];
+	$xatitle = $row['xatitle'];
+?><div class="xatheme">
+<?php if($xatitle!==""){ ?><div class="xaoption"><div class="xatitle"><?php echo $xatitle; ?></div></div><?php } ?>
+<embed flashvars="config=http://www.aparat.com/video/video/config/videohash/<?php echo $xatag; ?>" allowfullscreen="true" quality="high" name="aparattv" id="aparattv" style="" src="http://www.aparat.com/public/public/player/aparattv" type="application/x-shockwave-flash">
+<?php if(xasetv(3)!==""){ ?><div class="xaoption"><div class="xacanel"><a href="http://www.aparat.com/<?php echo xasetv(3); ?>">کانال ما</a></div></div><?php } ?>
+</div><?php
+}
+}
+?>
