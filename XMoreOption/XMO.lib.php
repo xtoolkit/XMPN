c684d660804a1a0b99c463a640af9bb259abbd0a
Arshen7 <delta.dorna@gmail.com>
2014-09-09 12:03:46 +0430

    update lib for Xsound functions
    update lib for Xsound functions

--- XMoreOption/XMO.lib.php
+++ XMoreOption/XMO.lib.php
@@ -24,6 +24,17 @@ $result = $db->sql_query("SELECT * FROM `" . $prefix . "_xaset` WHERE `xasid` =$
 }
 return $xasvalue;
 }
+function xssetv($nuim){
+global $prefix, $db, $dbname;
+$nuim=intval($nuim);
+$result = $db->sql_query("SELECT * FROM `" . $prefix . "_xsset` WHERE `xssid` =$nuim LIMIT 0 , 1");
+	while ($row = $db->sql_fetchrow($result)) {
+	mb_internal_encoding('UTF-8');
+	$xssvalue = $row['xssvalue'];
+}
+return $xssvalue;
+}
 if(xlbsetv(1)==0){$xlbtheme=xlbsetv(3);if($xlbtheme=="default"){}else{include("modules/XMoreOption/theme/XlinksBox/$xlbtheme/xlbtheme.php");}}else{function xlb_theme($id,$mod){}}
-if(xasetv(1)==0){$xatheme=xasetv(2);if($xlbtheme=="default"){}else{include("modules/XMoreOption/theme/Xaparat/$xatheme/xatheme.php");}}else{function xa_theme($id,$mod){}}
+if(xasetv(1)==0){$xatheme=xasetv(2);if($xatheme=="default"){}else{include("modules/XMoreOption/theme/Xaparat/$xatheme/xatheme.php");}}else{function xa_theme($id,$mod){}}
+if(xssetv(1)==0){$xstheme=xssetv(2);if($xstheme=="default"){}else{include("modules/XMoreOption/theme/Xsound/$xstheme/xstheme.php");}}else{function xs_theme($id,$mod){}}
 ?>
